974d853353d20c4809488b87e312770d707f57e0
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-07-15 15:31:20 +0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import DashBoardProject from "./Components/DashBoardProject";
+import { useState } from "react";
+import { db } from "./Firebase";
+import { useStateValue } from "./StateProvider";
 
 const App = () => {
+  const [{ user }] = useStateValue();
+
+  if (user) {
+    const getUserData = db.collection("users").doc(user.uid);
+    getUserData.get().then((doc) => {});
+  }
   return <DashBoardProject />;
 };
 
